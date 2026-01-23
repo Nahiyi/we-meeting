13206eefe0d31658c31dfce4d18d507aefd0da7a
liyihan <2872458411@qq.com>
2026-01-23 15:05:12 +0800

    升级fastjson(v2兼容包)到fastjson2

--- src/main/java/cn/clazs/easymeeting/interceptor/TokenInterceptor.java
+++ src/main/java/cn/clazs/easymeeting/interceptor/TokenInterceptor.java
@@ -4,7 +4,7 @@ import cn.clazs.easymeeting.entity.dto.UserTokenInfoDTO;
 import cn.clazs.easymeeting.entity.vo.ResponseVO;
 import cn.clazs.easymeeting.redis.RedisComponent;
 import cn.clazs.easymeeting.util.StringUtil;
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
